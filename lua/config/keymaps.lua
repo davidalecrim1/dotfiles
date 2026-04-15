5c822763203d4a8dd09533399b98590bea8587ca
David Alecrim <dsalecrim@outlook.com>
2026-04-15 11:56:55 -0300

    feat: add new keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+vim.keymap.set("n", "<leader>j", "}", { desc = "Next paragraph" })
+vim.keymap.set("n", "<leader>k", "{", { desc = "Previous paragraph" })
