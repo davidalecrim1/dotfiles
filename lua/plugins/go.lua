23c404c5de46d3be74ae8179b0209f5c11f19f87
David Alecrim <david.alecrim@alpaca.markets>
2026-08-31 14:46:49 -0300

    update lock and snacks plugin

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -13,4 +13,21 @@ return {
       },
     },
   },
+  {
+    "nvim-neotest/neotest",
+    ft = { "go", "gomod", "gowork", "gotmpl" },
+    opts = {
+      adapters = {
+        ["neotest-golang"] = {
+          go_test_args = { "-v", "-race", "-count=1", "-tags=integration" },
+          go_list_args = { "-tags=integration" },
+          dap_go_opts = {
+            delve = {
+              build_flags = { "-tags=integration" },
+            },
+          },
+        },
+      },
+    },
+  },
 }
