e44bb026734a28dfc73da4a9396f15cd10faff8d
David Alecrim <dsalecrim@outlook.com>
2026-09-14 13:27:16 -0300

    refactor: untrack nvim config (moved to dedicated repo)

--- nvim/lua/plugins/disable-markdown-lint.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "mfussenegger/nvim-lint",
-    opts = function(_, opts)
-      opts.linters_by_ft = opts.linters_by_ft or {}
-      opts.linters_by_ft.markdown = {}
-    end,
-  },
-}
--- nvim/lua/plugins/github-theme.lua
+++
@@ -1,6 +0,0 @@
-return {
-  {
-    "projekt0n/github-nvim-theme",
-    lazy = true,
-  },
-}
